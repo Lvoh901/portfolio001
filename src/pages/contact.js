ae8bc5802c4e3cb402305b8aad966dedf4092657
Elvis Odhiambo <ochiengelvis90@gmail.com>
2023-09-26 12:31:43 +0300

    faqs

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,42 +2,55 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div className="container contact">
-            <div className="contact-form-wrapper">
-                <form action="#" className="contact-form" id="contactForm">
-                    <h5 className="title">Get in Touch</h5>
-                    <p className="description">Feel free to contact me if you need any assistance or question.</p>
-                    <div>
-                        <input
-                            type="text"
-                            className="form-control rounded border-white mb-3 form-input"
-                            id="name"
-                            placeholder="Name"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="email"
-                            className="form-control rounded border-white mb-3 form-input"
-                            placeholder="Email"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <textarea
-                            id="message"
-                            className="form-control rounded border-white mb-3 form-text-area"
-                            rows="5"
-                            cols="30"
-                            placeholder="Message"
-                            required
-                        ></textarea>
-                    </div>
-                    <div className="submit-button-wrapper">
-                        <input type="submit" value="Send" />
-                    </div>
-                </form>
+        <div className="container">
+            <div className="container contact">
+                <div className="contact1">
+                    <form>
+                        <div className="group">
+                            <input type="text" required />
+                            <span className="highlight"></span>
+                            <span className="bar"></span>
+                            <label>Name</label>
+                        </div>
+
+                        <div className="group">
+                            <input type="text" required />
+                            <span className="highlight"></span>
+                            <span className="bar"></span>
+                            <label>Email</label>
+                        </div>
+
+                        <div className="group">
+                            <input type="text" required />
+                            <span className="highlight"></span>
+                            <span className="bar"></span>
+                            <label>Message</label>
+                        </div>
+
+                        <div className="submit-button-wrapper">
+                            <input type="submit" value="Send" />
+                        </div>
+                    </form>
+                </div>
+
+                <div className="contact2">
+                    <h4>Call Me</h4>
+                    <p>We can schedule a call to discuss your project and design. Reach Out on: 0712345678</p>
+                    <h4>Live Chat</h4>
+                    <p>We can schedule a zoom meeting or physical sit-down.</p>
+                </div>
+            </div>
+
+            <div className="faq text-white">
+                <div className="faq1">
+                    <p>FAQ</p>
+                    <h1 className="display-4 fw-bold">Frequently asked Questions.</h1>
+                </div>
+
+                <div className="faq2">
+                    <p className="fw-bold">Some Few Concerns you might have</p>
+                    <p>Frequently asked Questions.</p>
+                </div>
             </div>
         </div>
     );
